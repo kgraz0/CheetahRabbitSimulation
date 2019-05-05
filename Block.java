4c0544f918073f45fcad73533618185ae3fa3141
Karolis Grazulis <k.grazulis@yahoo.com>
2019-05-05 15:29:29 +0100

    Removed comments and unused parent and isVisited variables

--- Block.java
+++ Block.java
@@ -2,21 +2,13 @@ import java.util.ArrayList;
 
 class Block {
 
-	private int x, y, width, height; // x, y coordinates and width, height of each individual block
-	public static boolean isVisited; // boolean for individual block to see whether it has been visited 
-	public Block parent = null; // parent of the Block
-	//public static ArrayList<Block> neighbours = new ArrayList<Block>(); 
+	private int x, y, width, height;
 
-
-    /*
-    The block constructor that initializes X, Y, width, height and whether it has been visited
-    */
-	public Block(int newX, int newY, int newWidth, int newHeight, boolean newisVisited) {
+	public Block(int newX, int newY, int newWidth, int newHeight) {
 		this.x = newX;
 		this.y = newY;
 		this.width = newWidth;
 		this.height = newHeight;
-		this.isVisited = newisVisited;
 	}
 
 	public Block (Block newBlock) {
@@ -24,31 +16,26 @@ class Block {
 		this.y = newBlock.y;
 		this.width = newBlock.width;
 		this.height = newBlock.height;
-		this.isVisited = newBlock.isVisited;
 	}
 
 	public int getX() {
-		return x; // return x position for individual block
+		return x;
 	}
 	public int getY() {
-		return y; // return y position for individual block
+		return y;
 	}
 	public int getWidth() {
-		return width; // return the width of the individual block
+		return width;
 	}
 	public int getHeight() {
-		return height; // return the height of the individual block
-	}
-
-	public Block getParent(){
-		return parent; // return the parent Block
+		return height;
 	}
 
 	public void setX(int newX) {
-		this.x = newX; // set the x position for individual block
+		this.x = newX;
 	}
 
 	public void setY(int newY) {
-		this.y = newY; // set the y position for individual block
+		this.y = newY;
 	}
 }
